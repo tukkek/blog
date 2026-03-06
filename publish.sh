ce8ea6bf322c59080e16d9caba308ac38474c8b2
Alex Henry <tukkek@gmail.com>
2026-03-06 00:00:00 -0300

    Neocities publishing.

--- publish.sh
+++ publish.sh
@@ -5,3 +5,5 @@ jekyll build --config flags/blog.yaml,flags/git-hub.yaml
 git add docs/
 git commit --message "Publish site." docs/
 git push
+jekyll build --config flags/blog.yaml,flags/neocities.yaml
+neocities push docs/
