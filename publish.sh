3f5f934cd8a19a1f08bda08f5abaf40d0249e88d
Alex Henry <tukkek@gmail.com>
2026-03-06 00:00:00 -0300

    Build Neocities first so the repository is not left dirty compared to remote.

--- publish.sh
+++ publish.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 set -e
 
+jekyll build --config flags/blog.yaml,flags/neocities.yaml
+neocities push docs/
+
 jekyll build --config flags/blog.yaml,flags/git-hub.yaml
 git add docs/
 git commit --message "Publish site." docs/
 git push
-jekyll build --config flags/blog.yaml,flags/neocities.yaml
-neocities push docs/
+
+git status
